804b84215ab28c5a609ec8dd260a0a49357800b0
Lion-tang <361092772@qq.com>
2022-05-28 16:25:42 +0800

    concise contract

--- src/main/resources/contract/DetailCRUD.sol
+++ src/main/resources/contract/DetailCRUD.sol
@@ -6,24 +6,23 @@ import "./Table.sol";
 
 contract DetailCRUD {
     
-    event CreateResult(int256 count);
+
     event InsertResult(int256 count);
-    event UpdateResult(int256 count);
-    event RemoveResult(int256 count);
+    
 
     TableFactory tableFactory;
     string constant TABLE_name = "goods";
     constructor() public {
         tableFactory = TableFactory(0x1001); //The fixed address is 0x1001 for TableFactory
         // the parameters of createTable are tableuid,keyField,"vlaueFiled1,vlaueFiled2,vlaueFiled3,..."
-        tableFactory.createTable(TABLE_name, "uid", "hash,description");
+        tableFactory.createTable(TABLE_name, "uid", "attr");
     }
 
     //select records
     function select(string memory uid)
     public
     view
-    returns (string[] memory, string[] memory, string[] memory)
+    returns (string[] memory, string[] memory)
     {
         Table table = tableFactory.openTable(TABLE_name);
 
@@ -31,23 +30,20 @@ contract DetailCRUD {
 
         Entries entries = table.select(uid, condition);
         string[] memory goods_uid_bytes_list = new string[](uint256(entries.size()));
-        string[] memory hash_list = new string[](uint256(entries.size()));
-        string[] memory description_list = new string[](uint256(entries.size()));
+        string[] memory attr_list = new string[](uint256(entries.size()));
+      
 
         for (int256 i = 0; i < entries.size(); ++i) {
             Entry entry = entries.get(i);
-
             goods_uid_bytes_list[uint256(i)] = entry.getString("uid");
-            hash_list[uint256(i)] = entry.getString("hash");
-            description_list[uint256(i)] = entry.getString("description");
-            
+            attrlist[uint256(i)] = entry.getString("attr");
         }
 
-        return (goods_uid_bytes_list, hash_list, description_list);
+        return (goods_uid_bytes_list, attr_list);
     }
     
      //insert records
-    function insert(string memory uid, string memory hash, string memory description)
+    function insert(string memory uid, string memory attr)
     public
     returns (int256)
     {
@@ -55,45 +51,13 @@ contract DetailCRUD {
 
         Entry entry = table.newEntry();
         entry.set("uid", uid);
-        entry.set("hash", hash);
-        entry.set("description", description);
-
+        entry.set("attr", attr);
         int256 count = table.insert(uid, entry);
         emit InsertResult(count);
 
         return count;
     }
-    //update records
-    function update(string memory uid, string memory hash, string memory description)
-    public
-    returns (int256)
-    {
-        Table table = tableFactory.openTable(TABLE_name);
-
-        Entry entry = table.newEntry();
-        entry.set("hash", hash);
-        entry.set("description", description);
-
-        Condition condition = table.newCondition();
-        condition.EQ("uid", uid);
-
-        int256 count = table.update(uid, entry, condition);
-        emit UpdateResult(count);
-
-        return count;
-    }
-    //remove records
-    function remove(string memory uid) public returns (int256) {
-        Table table = tableFactory.openTable(TABLE_name);
-
-        Condition condition = table.newCondition();
-        condition.EQ("uid", uid);
-
-        int256 count = table.remove(uid, condition);
-        emit RemoveResult(count);
-
-        return count;
-    }
+   
     
    
 }
--- src/main/resources/contract/RecordCRUD.sol
+++ src/main/resources/contract/RecordCRUD.sol
@@ -6,92 +6,55 @@ import "./Table.sol";
 
 contract RecordCRUD {
     
-    event CreateResult(int256 count);
     event InsertResult(int256 count);
-    event UpdateResult(int256 count);
-    event RemoveResult(int256 count);
 
     TableFactory tableFactory;
     string constant TABLE_name = "records";
     constructor() public {
         tableFactory = TableFactory(0x1001); //The fixed address is 0x1001 for TableFactory
         // the parameters of createTable are tableuid,keyField,"vlaueFiled1,vlaueFiled2,vlaueFiled3,..."
-        tableFactory.createTable(TABLE_name, "main_tx_hash", "uid,locate");
+        tableFactory.createTable(TABLE_name, "uid", "attr");
     }
 
     //select records
-    function select(string memory main_tx_hash)
+    function select(string memory uid)
     public
     view
-    returns (string[] memory, string[] memory, string[] memory)
+    returns (string[] memory, string[] memory)
     {
         Table table = tableFactory.openTable(TABLE_name);
 
         Condition condition = table.newCondition();
 
-        Entries entries = table.select(main_tx_hash, condition);
+        Entries entries = table.select(uid, condition);
         string[] memory records_bytes_list = new string[](uint256(entries.size()));
-        string[] memory uid_list = new string[](uint256(entries.size()));
-        string[] memory locate_list = new string[](uint256(entries.size()));
+        string[] memory attr_list = new string[](uint256(entries.size()));
 
         for (int256 i = 0; i < entries.size(); ++i) {
             Entry entry = entries.get(i);
-            records_bytes_list[uint256(i)] = entry.getString("main_tx_hash");
-            uid_list[uint256(i)] = entry.getString("uid");
-            locate_list[uint256(i)] = entry.getString("locate");
+            records_bytes_list[uint256(i)] = entry.getString("uid");
+            attr_list[uint256(i)] = entry.getString("attr");
         }
 
-        return (records_bytes_list, uid_list, locate_list);
+        return (records_bytes_list, attr_list);
     }
     
      //insert records
-    function insert(string memory main_tx_hash, string memory uid, string memory locate)
+    function insert(string memory uid, string memory attr)
     public
     returns (int256)
     {
         Table table = tableFactory.openTable(TABLE_name);
 
         Entry entry = table.newEntry();
-        entry.set("main_tx_hash", main_tx_hash);
         entry.set("uid", uid);
-        entry.set("locate",locate);
-
-        int256 count = table.insert(main_tx_hash, entry);
+        entry.set("attr", attr);
+        int256 count = table.insert(uid, entry);
         emit InsertResult(count);
 
         return count;
     }
-    //update records
-    function update(string memory main_tx_hash, string memory uid, string memory locate)
-    public
-    returns (int256)
-    {
-        Table table = tableFactory.openTable(TABLE_name);
-
-        Entry entry = table.newEntry();
-        entry.set("uid", uid);
-        entry.set("locate", locate);
-
-        Condition condition = table.newCondition();
-        condition.EQ("main_tx_hash", main_tx_hash);
-
-        int256 count = table.update(main_tx_hash, entry, condition);
-        emit UpdateResult(count);
-
-        return count;
-    }
-    //remove records
-    function remove(string memory main_tx_hash) public returns (int256) {
-        Table table = tableFactory.openTable(TABLE_name);
-
-        Condition condition = table.newCondition();
-        condition.EQ("main_tx_hash", main_tx_hash);
-
-        int256 count = table.remove(main_tx_hash, condition);
-        emit RemoveResult(count);
-
-        return count;
-    }
+  
     
    
 }
